6d828bbb5e181e26f8b26db8f823c2c2b2739832
Asia Binek <joannabinek77@gmail.com>
2018-10-07 09:00:21 +0200

    DataFrame ver2

--- DataFrame/src/DF/DataFrame.java
+++ DataFrame/src/DF/DataFrame.java
@@ -1,76 +1,99 @@
 package DF;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class DataFrame {
-    int liczba_nazw;
-    int liczba_typow;
-    String  [] tablica_nazw = new String[liczba_nazw];
-    String  [] tabica_typow = new String[liczba_typow];
+    Kolumna[] kolumny;
+    int ilosc_wierszy;
 
-    ArrayList <Kolumna> kolumny;
-
-    public void DataFrame (String[] lista_nazw, String[]lista_typow){
-        liczba_nazw=lista_nazw.length;
-        liczba_typow=lista_typow.length;
-        tablica_nazw=lista_nazw;
-        tabica_typow=lista_typow;
-
-        // towrzy i dodaje kolejne kolumny do df
-        for (int i=0;i<tablica_nazw.length;i++){
-            Kolumna kol = new Kolumna(tablica_nazw[i],tabica_typow[i]);
-            kolumny.add(kol);
+    public DataFrame(String[] lista_nazw, String[] lista_typow) {
+        kolumny = new Kolumna[lista_typow.length];
+        for (int i = 0; i < lista_typow.length; i++) {
+            kolumny[i] = new Kolumna(lista_nazw[i], lista_typow[i]);
+        }
+        ilosc_wierszy = 0; // na początku brak danych == brak wierszy
     }
 
-   // public void zrobDF(){ // towrzy i dodaje kolejne kolumny do df
-     //   for (int i=0;i<tablica_nazw.length;i++){
-       //     Kolumna kol = new Kolumna(tablica_nazw[i],tabica_typow[i]);
-        //    kolumny.add(kol);
-        //}
-    }
-
-    public int size(){
-        Kolumna tmp = kolumny.get(0);
-        return tmp.kolumna.size();
-    }
-
-    public ArrayList get(String colname){//zwracającą kolumnę o podanej nazwie
-        int indeks=0;
-        int licznik = 0;
-        for (Kolumna x: kolumny){
-            if (x.nazwa == colname){
-                indeks = licznik;
+    public DataFrame(Kolumna[] kolumny) {
+        this.kolumny = kolumny;
+        ilosc_wierszy = kolumny[0].dane.size(); // liczba danych w dowolnej kolumnie, z zał mają mieć taką samą długość
+        for (Kolumna i : kolumny) {
+            if (ilosc_wierszy != i.dane.size()) {
+                throw new RuntimeException("Blad w dlugosci kolumn,kolumny nie sa rownej dlugosci!");
             }
-            else{licznik++;}
         }
-        Kolumna tmp  = kolumny.get(indeks);
-        return tmp.kolumna;
     }
 
-    public  DataFrame iloc(int i){ //zwracającą wiersz o podanym indeksie (jako nową DataFrame)
-        int licznik =0;
-        DataFrame nowa =  new DataFrame();
-        for (Kolumna j : kolumny){
-           Object tmp = j.kolumna.get(i);
-           nowa.tablica_nazw[licznik]=tmp.toString();
-           licznik++;
+
+    public int size() {
+        //Kolumna tmp = kolumny[0];
+        //return tmp.dane.size();
+        return ilosc_wierszy; // --> szybszy sposób
+    }
+
+    public Kolumna get(String colname) {//zwracającą kolumnę o podanej nazwie
+        //int indeks=0;
+        //int licznik = 0;
+        //for (Kolumna x: kolumny){
+        //   if (x.nazwa == colname){
+        //      indeks = licznik;
+        // }
+        // else{licznik++;}
+        // }
+        //Kolumna tmp  = kolumny[indeks];
+        //return tmp;
+        for (Kolumna i : kolumny) {
+            if (colname == i.nazwa) {  // --> szybszy sposób
+                return i;
+            }
         }
-        for (int m=0;m<nowa.tablica_nazw.length;m++){
-            Kolumna kol = new Kolumna(nowa.tablica_nazw[m],tabica_typow[m]);
-            nowa.kolumny.add(kol);
-        }
-        return nowa;
+        throw new NoSuchElementException("Nie ma takiej kolumny" + colname + "w tej Ramce Danych");
 
     }
 
-    public DataFrame iloc(int from,int to){ //zwracającą nową DataFrame z wierszami z podanego zakresu
-        DataFrame nowa =  new DataFrame();
-        for (int i=from;i<=to;i++){
-           DataFrame tmp = iloc(i);
-           nowa.kolumny.addAll(tmp.kolumny);
+    public DataFrame iloc(int from,int to){ //  zwraca nową DataFrame z wierszami z podanego zakresu
+        if (from<0 || from >= ilosc_wierszy){
+            throw new IndexOutOfBoundsException("Nie ma wiersza o numerze" + from);
         }
-        return nowa;
+
+        if(to<0 || to>=ilosc_wierszy){
+            throw new IndexOutOfBoundsException("Nie ma wiersza o numerze" + to);
+        }
+
+        if(to<from){
+            throw new IndexOutOfBoundsException("Niepoprawny zakres");
+        }
+
+        String[] typy = new String [kolumny.length]; //tablica na typy w nowej DF
+        String[] nazwy = new String [kolumny.length]; //tablica na nazwy w nowej DF
+
+        for (int i=0;i<kolumny.length;i++){ // wypełnianie tablic na podstawie starej DF
+            typy[i]= kolumny[i].typ;  // new String?
+            nazwy[i]=kolumny[i].nazwa;
+        }
+
+        DataFrame nowa_DF = new DataFrame(nazwy,typy); //nowa DF
+        Object[] nowe_wiersze = new Object[kolumny.length*(to-from+1)];  // inny zakres niz kolumny.lenght
+
+        for (int i=from;i<to;i++){
+            for (Kolumna k:kolumny){   // wypełnienie nowych_wierszy danymi na podstawie wierszy ze starej DF
+                nowe_wiersze[i]=k.dane.get(i);
+            }
+        }
+
+        for (int j=0;j<nowe_wiersze.length;j++){
+            for (Kolumna m:nowa_DF.kolumny){ //wstawanienie danych z nowych wierszy do pola Kolumny w nowej DF
+                m.dane.add(nowe_wiersze[j]);
+            }
+        }
+        return nowa_DF;
     }
+
+    public DataFrame iloc(int i){ //zwraca wiersz o podanym indeksie (jako nową DataFrame)
+        return iloc(i,i);
+    }
+
 }
 
 
--- DataFrame/src/DF/Kolumna.java
+++ DataFrame/src/DF/Kolumna.java
@@ -1,22 +1,26 @@
 package DF;
 
+import java.io.OptionalDataException;
 import java.util.ArrayList;
 
 public class Kolumna {
     String nazwa;
     String typ;
-    ArrayList<?> kolumna;
+    ArrayList dane;
 
-    public Kolumna(String name, String type){
-        nazwa=name;
-        typ=type;
-        try{
-            Class<?> nazwa_typu = Class.forName((typ));
-        }catch(ClassNotFoundException e){
-            System.out.println("Nie istnieje taki typ");
+    public Kolumna(String nazwa, String typ){
+        this.nazwa=nazwa;
+        this.typ=typ;
+        dane =  new ArrayList(10);
+    }
 
+    public void addElement(Object element){ // --> przenieść do DF
+        if (element.getClass().toString() == typ){
+            dane.add(element);
         }
-        kolumna =  new ArrayList<Object>(10);
+        else {
+            System.out.println("zły typ danych, nie psauje do tej kolmumny");}
+
     }
 
 }
